528dff85bdb26199cecb6b8db46757d18f99b0de
Doug856 <douglass@duncanfamily.co.za>
2025-10-28 13:50:18 +0200

    Added GUI Components

--- src/GUI/JFrame_GUI.java
+++ src/GUI/JFrame_GUI.java
@@ -4,35 +4,41 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JFrame_GUI extends JFrame {
+
+    private JLabel title;
+    private JTextField enterBar;
+    private JButton btnEnter;
+    private JLabel generatedNumber;
+    private JLabel confirmLabel;
+
+
     public JFrame_GUI() {
         super("Number Guesser");
-        setSize(new Dimension(400,400));
+        setSize(new Dimension(400, 400));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setVisible(true);
 
-
-
     }
 
-    public void addTitleLabel(){
-
+    public void addTitleLabel() {
+        title = new JLabel("Number Guesser");
     }
 
-    public void addEnterBar(){
-
+    public void addEnterBar() {
+        enterBar = new JTextField();
     }
 
-    public void addEnterBtn(){
-
+    public void addEnterBtn() {
+        btnEnter = new JButton("Enter");
     }
 
-    public void addGeneratedNumber(){
-
+    public void addGeneratedNumber() {
+        generatedNumber = new JLabel("");
     }
 
-    public void addConfirmLabel(){
-
+    public void addConfirmLabel() {
+        confirmLabel = new JLabel("");
     }
 
 
