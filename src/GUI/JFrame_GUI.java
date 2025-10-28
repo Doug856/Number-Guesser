60cbd2ff39f76596742241d213a9ceb014268ae0
Doug856 <douglass@duncanfamily.co.za>
2025-10-28 13:39:33 +0200

    Basic Code Structure Added

--- src/GUI/JFrame_GUI.java
+++ src/GUI/JFrame_GUI.java
@@ -1,4 +1,39 @@
 package GUI;
 
-public class JFrame_GUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class JFrame_GUI extends JFrame {
+    public JFrame_GUI() {
+        super("Number Guesser");
+        setSize(new Dimension(400,400));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+
+
+
+    }
+
+    public void addTitleLabel(){
+
+    }
+
+    public void addEnterBar(){
+
+    }
+
+    public void addEnterBtn(){
+
+    }
+
+    public void addGeneratedNumber(){
+
+    }
+
+    public void addConfirmLabel(){
+
+    }
+
+
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,9 @@
 package Main;
 
+import GUI.JFrame_GUI;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame_GUI jFG= new JFrame_GUI();
     }
 }
